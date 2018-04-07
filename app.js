69a3efa31f83a1545a0af19adfd1ee7fe86a29c8
Rak <lovetacirupeca@gmail.com>
2018-04-07 12:46:16 +0200

    feat: fix the favicon error

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
 app.use(cors(corsConfig));
 app.use(logger('dev'));
 app.use(bodyParser.json());
